f73d9bc1d9931ba1eb38f914413a261e352e5f1b
LeviPaiva <levicpg@gmail.com>
2022-09-28 20:52:34 -0300

    Criação das Cartas
    Criado a array das cartas com seus naipes

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Baralho.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Baralho.java
@@ -3,5 +3,20 @@ package proj.devops.bisca;
 
 public class Baralho {
 
+   
     
+
+Static public Cartas (){
+    
+  String[] CartaEspada = {"EA", "E7", "EK", "EJ", "EQ", "E6", "E5", "E4", "E3", "E2"};  
+  String[] CartaOuro = {"OA", "O7", "OK", "OJ", "OQ", "O6", "O5", "O4", "O3", "O2"};  
+  String[] CartaCopas = {"CA", "C7", "CK", "CJ", "CQ", "C6", "C5", "C4", "C3", "C2"};  
+  String[] CartaPaus = {"PA", "P7", "PK", "PJ", "PQ", "P6", "P5", "P4", "P3", "P2"};  
+
+
+
+}
+
+
+
 }

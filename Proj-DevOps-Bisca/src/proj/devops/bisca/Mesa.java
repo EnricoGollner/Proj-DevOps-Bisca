b554b9d537316941fb193c01150db94b10d65601
Augusto Oliveira <gutooliveira027@gmail.com>
2022-09-27 21:43:16 -0300

    Removendo os comentários desnecessários

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Mesa.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Mesa.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proj.devops.bisca;
 
-/**
- *
- * @author Augusto
- */
+
 public class Mesa {
     
     public static void main(String[] args) {
-     
-        
         
     }
     

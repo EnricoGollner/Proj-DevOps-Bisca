4347e667ab285b0595942d2360017c263d9f2225
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-09-28 21:54:48 -0300

    Alterando quantas cartas o jogador pode ter

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Jogador.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Jogador.java
@@ -3,6 +3,6 @@ package proj.devops.bisca;
 import java.util.ArrayList;
 
 public class Jogador {
-    static ArrayList<String> Jogador1 = new ArrayList<>(); 
-    static ArrayList<String> Jogador2 = new ArrayList<>(); 
+    static ArrayList<String> Jogador1 = new ArrayList<>(3);
+    static ArrayList<String> Jogador2 = new ArrayList<>(3);
 }

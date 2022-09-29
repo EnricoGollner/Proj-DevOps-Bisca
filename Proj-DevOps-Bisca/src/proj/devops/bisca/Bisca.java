2a23c12f2a7305c50b76e139635a1093ed83f981
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-09-29 01:59:46 -0300

    Criando cartas e melhorando a estrutura

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Bisca.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Bisca.java
@@ -1,20 +1,28 @@
 package proj.devops.bisca;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Guto
  */
 public class Bisca {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    // Criando jogadores:
+    public ArrayList<String> Jogador1 = new ArrayList<>(3);
+    public ArrayList<String> Jogador2 = new ArrayList<>(3);
+    
+    // Criando as cartas
+    public static String[] nipes = {"Ouro", "Copas", "Espadas", "Paus"};
+    public static String[] faces = {"As", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "Valete", "Dama", "Rei"};
+
+    public static void Mesa(String[] args) {
         // TODO code application logic here
         
         
         
+        
     }
-    
-}
 
+}

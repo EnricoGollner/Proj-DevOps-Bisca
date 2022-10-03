5671354cf64bb3dd53df563b3d7b9c73b6b2bb53
1Miuka <viniciusgsouto@gmail.com>
2022-10-03 14:49:57 -0300

    Adicionando Regra 2 e 3

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Regras.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Regras.java
@@ -15,5 +15,15 @@ public class Regras {
                 resultadoJogador2++
             }
         }
+        
+        // Regra 2 e 3 ( Precisa arrumar nome das variáveis e fazer com que olhe quem jogou primeiro ).
+        
+        if ( naipe.Jogador1 != naipe.Jogador2 ) {
+            if ( face.Jogador1 != trunfo && face.Jogador2 != trunfo){
+                // Quem jogou primeiro ganha pontuação ( não sei verificar quem jogou primeiro )
+            } else if ( face.Jogador1 == trunfo || face.Jogador2 == trunfo) {
+                // O jogaor que tiver o trunfo ganha pontuação
+            }
+        }
     }
 }

59a4a74b5f09f0b2e935296036b65626edbcc0e5
1Miuka <viniciusgsouto@gmail.com>
2022-10-03 14:47:45 -0300

    Adicionando uma regra.

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Regras.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Regras.java
@@ -1,6 +1,19 @@
 package proj.devops.bisca;
 
+import static proj.devops.bisca.Baralho.faces;
+import static proj.devops.bisca.Baralho.nipes;
 
-public class Regras {
-    
+public class Regras {  
+    public static void main(String[] Regras) {
+        
+        // Regra 1 || Se os nipes são iguais, a face com maior número pontua. ( precisa arrumar o nome das variaveis )
+        
+        if ( naipe.Jogador1 == naipe.Jogador2 ){
+            if ( face.Jogador1 > face.Jogador2 ){
+                resultadoJogador1++
+            }else {
+                resultadoJogador2++
+            }
+        }
+    }
 }

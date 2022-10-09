bcde041b366de69342fab45a620aeee288c1adfb
Glauciar <glauciar_21@hotmail.com>
2022-10-08 22:49:42 -0300

    na classe bisca acrescentei os codigos referente ao comando apos as jogadas comprar cartas e acertei o comando na linha 7 tambem na classe bisca

--- Proj-DevOps-Bisca/src/proj/devops/bisca/Bisca.java
+++ Proj-DevOps-Bisca/src/proj/devops/bisca/Bisca.java
@@ -4,17 +4,26 @@ import java.util.ArrayList;
 
 
 // Classe responsável por exibir as partidas e o resultado final
-public class Bisca() {
+public class Bisca {
     public static void main(String[] args) {
     
         // Pegando os jogadores já com as cartas na mão (embaralhadas)
         ArrayList<String> Jogador1 = Jogador.Jogador1();
-        ArrayList<String> Jogador2 = Jogador.Jogador2();   
+        ArrayList<String> Jogador2 = Jogador.Jogador2();
         
         // Jogadas
         
-        // Após as jogadas Comprar Cartas
         
+    }
+        
+        // Após as jogadas Comprar Cartas
+        if (mesa.temcartas ()) {
+            vJogadores[J1].pegarcartas (distribuir:mesa.distribuir ());
+            vJogadores[J2].pegarcartas (distribuir:mesa.distribuir ());
+        }
+            
+            
+      
 
     }
 
